f96e9cfb0411f1701e742457387e621af159bc4e
davidfung <davidfung@amgcomputing.com>
2023-08-12 15:12:14 -0700

    Add problem 4.2

---
+++ problems/p0402.py
@@ -0,0 +1,54 @@
+# sp.py parser
+# Grammar:
+#    <S> -> 'a' <B> 'd'
+#    <B> -> ('b''b')* ['c']
+import sys   # needed to access command line arg
+
+#global variables
+tokenindex = -1
+token = ''
+
+def main():
+   try:
+      parser()         # call the parser
+   except RuntimeError as emsg:
+      print(emsg)
+
+def advance():
+   global tokenindex, token
+   tokenindex += 1     # move tokenindex to next token
+   # check for null string or end of string
+   if len(sys.argv) < 2 or tokenindex >= len(sys.argv[1]):
+      token = ''      # signal the end by returning null string
+   else:
+      token = sys.argv[1][tokenindex]
+
+def consume(expected):
+   if expected == token:
+      advance()
+   else:
+      raise RuntimeError('Expecting ' + expected)
+
+def parser():
+   advance()   # prime token with first character
+   S()         # call function for start symbol
+   # test if end of input string
+   if token != '': 
+      print('Garbage following <S>-string')
+   
+#    <S> -> 'a' <B> 'd'
+#    <B> -> ('b''b')* ['c']
+def S():
+   consume('a')
+   B()
+   consume('d')
+
+def B():
+   if token == 'b':
+      while token == 'b':
+         consume('b')
+         consume('b')
+   if token == 'c':
+      advance()
+
+main()
