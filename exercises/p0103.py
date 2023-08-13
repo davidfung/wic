cc92a6a6fbb22e9a61f75d86b7df72464ec1db2c
davidfung <davidfung@amgcomputing.com>
2023-08-13 00:23:21 -0700

    Add Problem 4.3

--- exercises/p0103.py
+++
@@ -1,5 +0,0 @@
-with open("p0103.txt", "r") as file:
-    data = file.read().split()
-
-for x in list(filter(lambda x: not x.isnumeric(), data)): print(x)
-for x in list(filter(lambda x: x.isnumeric(), data)): print(x)
