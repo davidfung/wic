734505ef679649f9e97a46e22cac54aac65f57ba
David Fung <davidfung@amgcomputing.com>
2024-04-09 19:38:20 -0700

    Add solution 11.6

--- problems/p1106.s
+++ problems/p1106.s
@@ -1,12 +1,14 @@
-                                @ p1101.s
+                                @ p1106.s
           .text                 @ start of read-only segment
           .global _start
+y:        .word 43            @ the variable x
+          .space 4084
 _start:
-          ldr r0, x             @ load r0 from x
+          ldr r0, y             @ load r0 from x
           mov r7, #1            @ mov 1 into r7
           svc 0                 @ supervisor call to terminate program
 
 @         .space 36000
-          .space 39999
+          .space 4092
 @         .space 39999
-x:        .word 6502            @ the variable x
+x:        .word 42            @ the variable x
