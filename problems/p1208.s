1868ae474f9dd29745565645f74865f4d6b570b6
David Fung <davidfung@amgcomputing.com>
2024-04-19 21:41:56 -0700

    Add solution 12.8

---
+++ problems/p1208.s
@@ -0,0 +1,30 @@
+                             @ p1208.s
+          .global main       @ scanf and printf assumed global 
+          .text              @ start of read-only segment
+main:
+          push {lr}          @ save lr by pushing onto stack
+          
+          ldr r0, =.z0       @ get address of string
+          ldr r1, =x         @ get address of x
+          bl  scanf          @ call scanf
+
+          ldr r10, =x
+          ldr r10, [r10]
+          mov r11, #1
+          mov r1, #0
+loopstart:
+          tst r10, #1
+          beq even
+          add r1, r10
+even:
+          subs r10, r10, r11
+          bne loopstart
+ 
+          ldr r0, =.z1       @ get address of string
+          bl  printf         @ call printf
+
+          pop {pc}           @ pop saved lr into pc
+          .data              @ start of read/write segment
+.z0:      .asciz "%d"        @ null-terminated ASCII string
+.z1:      .asciz "sum=%d\n"  @ null-terminated ASCII string
+x:        .word 0
